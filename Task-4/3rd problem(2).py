58630759b6fada860e60ac788bd7d6f9b76e2c05
Harini-54 <99632688+Harini-54@users.noreply.github.com>
2022-02-23 11:37:04 +0530

    Delete 3rd problem(2).py

--- Task-4/3rd problem(2).py
+++
@@ -1,6 +0,0 @@
-x = [ [1, 'Mahesh', 97],
-     [2, 'Sweety', 77],
-     [3, 'Harini', 87]]
-print("{:<10} {:<10} {:<10} ".format('Roll no.', 'Name',  'Marks'))
-for i in range(3):
-    print(x[0][i], end=" "*10)
